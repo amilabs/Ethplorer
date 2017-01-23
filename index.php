0d7678f63e32967cec3af6d55a38cb7dfb33937b
ArtemKo7v <artemk@211.ru>
2017-01-23 18:07:09 +0700

    Version increased

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$codeVersion = "104";
+$codeVersion = "105";
 
 require dirname(__FILE__) . '/service/lib/ethplorer.php';
 $esCfg = require_once dirname(__FILE__) . '/service/config.php';
