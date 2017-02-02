5032300c345cbba71bc7be0a3a79cf040d1f24e7
Lexa M <lexa-m@users.noreply.github.com>
2017-02-02 16:47:43 +0700

    Chart customization.

--- index.php
+++ index.php
@@ -130,6 +130,7 @@ if($error){
                         margin-top: 0 !important;
                         margin-bottom: 0 !important;
                         padding: 0 !important;
+                        max-width: 86% !important;
                     }
                 </style>
                 <div>
