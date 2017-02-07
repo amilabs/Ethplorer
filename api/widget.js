4846bced93abba5ff46d2ffc50f5b494abe2d1d5
Lexa M <lexa-m@users.noreply.github.com>
2017-02-07 18:51:07 +0700

    Ethplorer widgets.

--- api/widget.js
+++ api/widget.js
@@ -604,7 +604,7 @@ ethplorerWidget.Type['dailyTX'] = function(element, options, templates){
             },
             hAxis : {
                 title: '90 days token operations chart',
-                titleTextStyle: {color: '#FFF'},
+                titleTextStyle: {color: '#B0B0B0'},
                 textPosition: 'out',
                 textStyle: {color: '#FFF'},
                 slantedText: false,
@@ -619,7 +619,7 @@ ethplorerWidget.Type['dailyTX'] = function(element, options, templates){
             },
             vAxis: {
                 title: 'Token operations',
-                titleTextStyle: {color: '#FFF'},
+                titleTextStyle: {color: '#B0B0B0'},
                 textStyle: {color: '#FFF'},
                 minValue: 0,
                 gridlines: {
