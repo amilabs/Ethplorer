34ed81eb4aa6bc445ca2d330d4b3457d97ea023e
ArtemKo7v <artemk@211.ru>
2017-02-20 19:07:06 +0700

    getTokenInfo API test

--- tests/Ethplorer/Api_Test.php
+++ tests/Ethplorer/Api_Test.php
@@ -10,6 +10,8 @@ use PHPUnit_Framework_TestCase;
  */
 class Api_Test extends PHPUnit_Framework_TestCase{
 
+    protected $url = 'https://api.ethplorer.io/';
+
     /**
      * @return void
      */
@@ -20,7 +22,45 @@ class Api_Test extends PHPUnit_Framework_TestCase{
     /**
      * @covers
      */
-    public function testCheckServerConfig(){
-        $this->assertTrue(TRUE, TRUE);
+    public function testGetTokenInfo(){
+        $cmd = 'getTokenInfo';
+        $testAddress = '0xFf71Cb760666Ab06aa73f34995b42dd4b85ea07b';
+
+        $aResult = $this->rq($cmd, $testAddress, array('apiKey' => 'freekey'));
+
+        $this->assertTrue(
+            is_array($aResult),
+            sprintf(
+                "Invalid response received:\n%s",
+                var_export($aResult, TRUE)
+            )
+        );
+        $aMandatoryFields = array("address", "name", "decimals", "symbol", "totalSupply", "owner", "totalIn", "totalOut", "holdersCount", "issuancesCount", "countOps");
+        foreach($aMandatoryFields as $field){
+            $this->assertTrue(
+                isset($aResult[$field]),
+                sprintf(
+                    "Field {$field} not present in response:\n%s",
+                    var_export($aResult, TRUE)
+                )
+            );
+        }
+        $this->assertEquals(strtolower($testAddress), $aResult['address']);
+        $this->assertEquals('THBEX', $aResult['name']);
+        $this->assertEquals("4", $aResult['decimals']);
+        $this->assertEquals('THBEX', $aResult['symbol']);
+    }
+
+    protected function rq($cmd, $subject = '', array $aParameters = array()){
+        $url = $this->url . $cmd;
+        if($subject){
+            $url = $url . '/' . $subject;
+        }
+        if(!empty($aParameters)){
+            $url = $url . '?' . http_build_query($aParameters);
+        }
+        $json = file_get_contents($url);
+        $aResult = json_decode($json, TRUE);
+        return $aResult;
     }
 }
