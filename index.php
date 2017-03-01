780bd9ce32e4eacc9584819b696b6268d3a49be8
Lexa M <lexa-m@users.noreply.github.com>
2017-03-01 21:02:41 +0700

    Ethplorer CSV export.

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ if($error){
         die();
     }
 }
-$csvExport = ' <span class="export-csv"><a class="download" href="javascript:void(0)" data-address="' . $rParts[2] . '">Export as CSV</a></span>';
+$csvExport = ' <span class="export-csv-spinner"><i class="fa fa-spinner fa-spin"></i> Export...</span><span class="export-csv"><a class="download" href="javascript:void(0)" data-address="' . $rParts[2] . '">Export as CSV</a></span>';
 ?><!DOCTYPE html>
 <html>
 <head>
