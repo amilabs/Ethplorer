229f9613016d2e08a66a6559b397bd7106c79b4e
ArtemKo7v <artemk@211.ru>
2017-03-01 14:43:07 +0700

    Disqus under footer bug fixed

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$codeVersion = "125";
+$codeVersion = "126";
 
 require dirname(__FILE__) . '/service/lib/ethplorer.php';
 $esCfg = require_once dirname(__FILE__) . '/service/config.php';
