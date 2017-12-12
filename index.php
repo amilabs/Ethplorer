9e923bd04f02de2af39013612b0e46d04d7bbad2
Lexa M <lexa-m@users.noreply.github.com>
2017-12-12 17:36:22 +0700

    Code version increased.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$codeVersion = "175";
+$codeVersion = "176";
 
 $aConfig = require dirname(__FILE__) . '/service/config.php';
 require dirname(__FILE__) . '/service/lib/ethplorer.php';
